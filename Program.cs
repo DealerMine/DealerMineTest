67bdef2ca0fc407dd9430183a8af3fd33e28b8db
dmadminmkelly <dmadminmkelly@DM-STG02>
2018-10-11 08:29:08 -0300

    te

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DealerMineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Console.WriteLine("Test");
+           
+            Thread.Sleep(10000);
+        }
+    }
+}
+//C:\Users\dmadminmkelly\source\repos\DealerMineTest\DealerMineTest\
